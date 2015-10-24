a76ea2811fabbcd9a3e3c898fe909e26974897f7
Leonid Zolotarev <leoz.android@gmail.com>
2015-10-24 12:03:01 -0400

    Add Ionic image build

--- build_images.sh
+++ build_images.sh
@@ -4,3 +4,4 @@ docker build --tag=base_ubuntu base_ubuntu/
 docker build --tag=base_runner base_runner/
 docker build --tag=base_runner_java base_runner_java/
 docker build --tag=base_runner_android base_runner_android/
+docker build --tag=base_runner_ionic base_runner_ionic/
